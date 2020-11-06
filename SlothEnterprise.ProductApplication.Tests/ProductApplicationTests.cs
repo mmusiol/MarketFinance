5ecf87de28a80cd0ba3ece49b8064e6e0d58c1cb
Michał Musioł <ciastczan@gmail.com>
2020-11-06 17:01:32 +0100

    Initial solution loaded

---
+++ SlothEnterprise.ProductApplication.Tests/ProductApplicationTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace SlothEnterprise.ProductApplication.Tests
+{
+    public class ProductApplicationTests
+    {
+
+        [Fact]
+        public void ThisIsTrue()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
